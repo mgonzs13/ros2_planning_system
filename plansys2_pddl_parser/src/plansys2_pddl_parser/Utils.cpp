1e606f83df3ce59616996b763445fd37dab2b0bf
Miguel Ángel González Santamarta <mgons@unileon.es>
2025-11-30 22:56:55 +0100

    commenting std::cerr to avoid spamming stderr when adding goals in terminal

--- plansys2_pddl_parser/src/plansys2_pddl_parser/Utils.cpp
+++ plansys2_pddl_parser/src/plansys2_pddl_parser/Utils.cpp
@@ -281,9 +281,10 @@ std::tuple<uint8_t, int> getExpr(const std::string & input)
     }
   }
 
-  if (expr_type == plansys2_msgs::msg::Node::UNKNOWN) {
-    std::cerr << "getExpr: Error parsing expresion [" << input << "]" << std::endl;
-  }
+  // Commented out to avoid spamming stderr when adding goals in terminal
+  // if (expr_type == plansys2_msgs::msg::Node::UNKNOWN) {
+  //   std::cerr << "getExpr: Error parsing expresion [" << input << "]" << std::endl;
+  // }
 
   return std::make_tuple(expr_type, first);
 }
